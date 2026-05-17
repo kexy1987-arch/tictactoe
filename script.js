9ed6b9b85b02d38df936e329a1207191de03dbd5
László <kexy1987@gmail.com>
2026-05-17 16:21:04 +0100

    css and js to trigger player input and winner container

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const boardEl = document.getElementById("board");
-//let board = new Array(9).fill(null);
 
-function Player(name, marker){
+function Player(name, marker, color){
     return{
         name,
         marker,
+        color,
         score: 0
     }
 }; 
@@ -17,8 +17,15 @@ const GetPlayersName = (function() {
     const playBtn = document.querySelector("button[type=submit]")
     playBtn.addEventListener("click", (e) => {
         e.preventDefault();
-        Gameboard.player1.name = player1Input.value;
-        Gameboard.player2.name = player2Input.value;
+        const form = document.getElementById("get-player");
+        form.classList.add("fade");
+        void form.offsetWidth;
+        console.log(e.target.parentElement)
+        setTimeout(() => {
+            form.hidden = true;
+        }, 2000);
+        Gameboard.player1.name = capitalize(player1Input.value);
+        Gameboard.player2.name = capitalize(player2Input.value);
         player1Name.textContent = Gameboard.player1.name;
         player2Name.textContent = Gameboard.player2.name;
     })
@@ -26,8 +33,8 @@ const GetPlayersName = (function() {
 
 const Gameboard = (function() {
     const board = new Array(9).fill(null);
-    const player1 = new Player("Player1", "x");
-    const player2 = new Player("Player2", "o");
+    const player1 = new Player("Player1", "x", "orange");
+    const player2 = new Player("Player2", "o", "orangered");
 
     let actualPlayer = player1;
     let gameOver = false;
@@ -78,6 +85,7 @@ function win(board) {
     for (const [a, b, c] of winningCombos) {
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
             document.getElementById("winner").textContent = Gameboard.actualPlayer.name;
+            document.getElementById("winner-container").hidden = false;
             Gameboard.gameOver = true;
             return;
         }
@@ -92,8 +100,28 @@ function round(e, index) {
         return;
     } else {
         Gameboard.board[index] = Gameboard.actualPlayer.marker;
-        e.target.textContent = Gameboard.actualPlayer.marker;
-
+        e.target.style.backgroundColor = Gameboard.actualPlayer.color;
+        if (Gameboard.actualPlayer.marker === "o"){
+            const marker = document.createElement("div");
+            marker.classList.add(`${Gameboard.actualPlayer.marker}`);
+            e.target.appendChild(marker);
+            setTimeout(() => marker.classList.add("grow"), 100);
+        }else{
+            const firstPart = document.createElement("div");
+            firstPart.classList.add("first-part");
+            const secondPart = document.createElement("div");
+            secondPart.classList.add("second-part");
+            const marker = document.createElement("div");
+            marker.classList.add(`${Gameboard.actualPlayer.marker}`);
+            e.target.appendChild(marker);
+            marker.appendChild(firstPart);
+            marker.appendChild(secondPart);            
+            setTimeout(() => {
+                firstPart.classList.add("grow1");
+                secondPart.classList.add("grow2");
+                
+            }, 100);
+        }
         if (win(Gameboard.board) !== undefined) {
             return;
         };
@@ -112,110 +140,10 @@ function resetBoard() {
     boardEl.innerHTML = "";
     gameOver = false;
     Gameboard.createBoard();
+    document.getElementById("winner-container").hidden = false;
 };
 
 function capitalize(str) {
     if (!str) return "";
     return str[0].toUpperCase() + str.slice(1);
 };
-
-
-
-
-
-/*
-function getPlayersName(){
-    const player1Name = document.getElementById("player1-name");
-    const player2Name = document.getElementById("player2-name");
-    const player1Input = document.getElementById("player1");
-    const player2Input = document.getElementById("player2");
-    const playBtn = document.querySelector("button[type=submit]")
-    playBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        const player1 = new Player(capitalize(player1Input.value), "x", "red");
-        const player2 = new Player(capitalize(player2Input.value), "o", "green");
-        player1Name.textContent = player1.name;
-        player2Name.textContent = player2.name;
-        game(player1, player2)
-        console.log(player1Input.value)
-        console.log(player2)
-    })
-}
-
-let gameOver = false;
-
-function win(board){
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6] 
-    ];
-
-    for (const [a, b, c] of winningCombos) {
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            gameOver = true;
-            return board[a] + " wins!";
-        }
-    }
-};
-
-let actualPlayer;
-
-function game(player1, player2){
-
-    actualPlayer = player1;
-    
-    function round(e, index) {
-        if(gameOver){
-            return;
-        }
-        if (board[index] !== null){
-            console.log(actualPlayer)
-            return;
-        }else{ 
-            board[index] = actualPlayer.marker;
-            e.target.style.backgroundColor = actualPlayer.color;
-            e.target.textContent = actualPlayer.marker;
-            
-            if (win(board) !== undefined) {
-                document.getElementById("winner").textContent = actualPlayer.name;
-                return
-            };
-            actualPlayer = actualPlayer === player1 ? player2 : player1;
-        };
-    }
-    
-    function createBoard() {
-        
-        board.forEach((element, index) => {
-            const square = document.createElement("div");
-            square.classList.add("square");
-            square.id = index
-            boardEl.appendChild(square);
-            square.addEventListener("click", (e) => {
-                round(e, index);
-            })
-        });
-    };
-
-    const clearBoardBtn = document.getElementById("clear-board-btn");
-    clearBoardBtn.addEventListener("click", () => resetBoard(player1));
-
-    function resetBoard(player1) {
-        board = new Array(9).fill(null);
-        actualPlayer = player1;
-        boardEl.innerHTML = "";
-        createBoard();
-        gameOver = false;
-        console.log(board);
-    }
-
-    createBoard();
-}
-
-*/
